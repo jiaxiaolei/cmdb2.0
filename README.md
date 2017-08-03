a7d64f10a4d1ba7f6eef10848983811aeeb0ccbb
jiaxiaolei <jiaxiaolei@syswin.com>
2017-08-03 13:12:55 +0000

    Feature:/ add file BugId:/ Description: 1. add gitignore              2. rich README.md              3. add conf.d

--- README.md
+++ README.md
@@ -4,6 +4,13 @@
 python:
 2.7.13
 
+Web framework:
+Django
+
 
 $ mkvirtualenv py2.7.13cmdb2.0 -p /root/.pyenv/versions/2.7.13/bin/python --no-site-packages
+
+
+
+$ pep8 server.py
 
--- misc/orientedb.py
+++ misc/orientedb.py
@@ -9,6 +9,7 @@ from tornado.options import parse_command_line
 import pyorient
 
 HOST = 'localhost'
+# HOST = '172.28.32.49'
 PORT = 2424
 USER = 'root'
 PASSWORD = 'root'
@@ -19,17 +20,26 @@ def main():
     client = pyorient.OrientDB(HOST, PORT)
     session_id = client.connect(USER, PASSWORD)
     
-    flag = client.db_exists(DB_NAME, pyorient.STORAGE_TYPE_MEMORY)
-    logging.info('flag:%s' % flag)
-    
-    if flag:
+    is_exist = client.db_exists(DB_NAME, pyorient.STORAGE_TYPE_MEMORY)
+    if is_exist:
         logging.info("Database: %s has exist, do not creat it again.", DB_NAME)
     else: 
         client.db_create(DB_NAME, pyorient.DB_TYPE_GRAPH, pyorient.STORAGE_TYPE_MEMORY)
+        logging.info("Database: %s is created", DB_NAME)
 
+    db_list = client.db_list()
+    logging.info("Database list: %s is created", db_list)
+
+    #db_size = client.db_size()
+    #logging.info("The size of the db is: %s", db_size)
+    ## NOTE: You must have an opened database to issue this command
+
+    #db_count_records = client.db_count_records()
+    #logging.info("Database count of records is: %s", db_count_records)
+    ### NOTE: You must have an opened database to issue this command
+    
 
 if __name__ == '__main__':
     parse_command_line()
-    
    
     main()
