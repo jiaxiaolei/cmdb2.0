ddb22d19adda3c62f521c928a19160d270b64347
jiaxiaolei <jiaxiaolei@syswin.com>
2017-08-08 07:10:32 +0000

    feat: add interface: index, test

--- uweb/views.py
+++ uweb/views.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.http import JsonResponse
+from django.http import HttpResponse
+
 # Create your views here.
+ 
+def index(request):
+    return HttpResponse(u"Welcome to cmdb2.0!")
+
+def test(request):
+    ret = dict(code=0,
+               message='success',
+               data={})
+    return JsonResponse(ret)
