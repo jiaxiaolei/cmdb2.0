b07a60e82532b46a8b212cbab077787195c1a461
jiaxiaolei <jiaxiaolei@cicaero.com>
2017-08-08 11:43:31 +0800

    chore(<libs>): add constants
    add constants folder. constants should be included in it

---
+++ libs/constants/HTTP.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from utils.dotdict import DotDict
+
+METHOD = DotDict(DELETE="DELETE",
+                 POST="POST",
+                 PUT="PUT",
+                 GET="GET")
+
+ASYNC_REQUEST_TIMEOUT = 45
+CONNECT_TIMEOUT = 30
